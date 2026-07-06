19ad9ec7672160bf8daee4701bb204bce77f9c7d
Anup Patwa <anuppatwa1090@gmail.com>
2026-07-06 21:24:18 +0530

    Add program to calculate average of three numbers

---
+++ Average three numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c, Average;
+
+    cout << "Enter Three Number:";
+    cin >> a >> b >> c;
+
+    Average = (a + b + c) / 3;
+    cout << "The Average is:" << Average << endl;
+    return 0;
+}
